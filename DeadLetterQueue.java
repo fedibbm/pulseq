f6397aeb1d09d1456e438f4fceccb9fa4bffcbe6
fedibbm <benbrahimfedi4@gmail.com>
2026-07-08 14:05:00 +0100

    feat: add per-topic dead-letter queue

---
+++ DeadLetterQueue.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class DeadLetterQueue {
+    private final String sourceTopic;
+    private final LinkedList<Message> messages;
+
+    DeadLetterQueue(String sourceTopic){
+        this.sourceTopic = sourceTopic;
+        this.messages = new LinkedList<>();
+    }
+
+    void add(Message message){
+        this.messages.addLast(message);
+    }
+
+    List<Message> list(){
+        return new ArrayList<>(this.messages);
+    }
+
+    public String getSourceTopic(){
+        return this.sourceTopic;
+    }
+    public int size(){
+        return this.messages.size();
+    }
+}
